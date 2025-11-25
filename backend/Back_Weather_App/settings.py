c633722579054406c6d0b7bb0767d10a8664ec9d
Aldmor <kubziom.pr@gmail.com>
2025-11-25 13:02:45 +0100

    Add authentication app

--- backend/Back_Weather_App/settings.py
+++ backend/Back_Weather_App/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'weather_api',
-    'frontend_api'
+    'frontend_api',
 ]
 
 MIDDLEWARE = [
