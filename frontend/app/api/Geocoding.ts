59cf0993a066160c86224314749ceb5906971e27
Aldmor <kubziom.pr@gmail.com>
2025-11-29 16:01:11 +0100

    Geo API front

---
+++ frontend/app/api/Geocoding.ts
@@ -0,0 +1,12 @@
+const API_URL = "http://localhost:5050/api/v1/geocoding";
+
+export async function getCoordinatesByLocation(locationName: string) {
+  const res = await fetch(`${API_URL}/?q=${encodeURIComponent(locationName)}`);
+  
+  if (!res.ok) {
+    throw new Error("Failed to fetch coordinates");
+  }
+  
+  return res.json();
+}
+
