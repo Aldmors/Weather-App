6c384efdd8ac5be40dd0bcd1e29044705e012cb9
Aldmor <kubziom.pr@gmail.com>
2025-11-29 21:34:14 +0100

    Add admin model view

--- backend/frontend_api/admin.py
+++ backend/frontend_api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import FavoriteLocations
 
-# Register your models here.
+
+class FavoriteLocationsAdmin(admin.ModelAdmin):
+    list_display = ('location_name', 'owner', 'date_added')
+    list_filter = ('location_name', 'owner', 'date_added')
+    search_fields = ('location_name', 'owner__username')
+    date_hierarchy = 'date_added'
+
+admin.site.register(FavoriteLocations, FavoriteLocationsAdmin)
